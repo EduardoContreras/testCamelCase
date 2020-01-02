710f8f94442a03f049f511dcaec2f336d07e5597
EduardoContreras <eduardo.andre.contreras@gmail.com>
2020-01-02 18:42:13 -0300

    ...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const xmlparser = require('express-xml-bodyparser');
 const bodyParser = require('body-parser');
-const { every, isObject, isArray, map, mapValues, camelCase, has, eq } = require('lodash');
+const { every, isObject, isArray, map, mapValues, camelCase, has, eq, filter, isEqual, toPlainObject } = require('lodash');
 const camelcaseKeys = require('camelcase-keys');
 
 app.use(bodyParser.json({limit: '50mb', extended: true}));
@@ -20,6 +20,21 @@ const toCamelCase = items =>
       return trnsfrm;
     } else if (isObject(item) && !has(item, 'anyOf')) {
       return toCamelCase(item);
+    } else if (has(item, 'anyOf')) {
+      const retrn = map(item.anyOf, current => {
+        if (!isEqual(current, { type: 'null' })) {
+          return current;
+        } else {
+          return {nullable: "true"};
+        }
+      });
+
+      let obj = {};
+      const rtrn = retrn.reduce(function(result, item, index) {   
+        obj = Object.assign({}, obj, item);     
+      }, []);
+
+      item = obj;
     }
     return item;
   });
